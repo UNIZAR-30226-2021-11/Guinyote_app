f32cff4352e3e29f2985fd0c246469f4c21db759
samueltorresfau <73356281+samueltorresfau@users.noreply.github.com>
2021-04-11 22:46:43 +0200

    JuegoActivity creada
    Ahora mismo no es más que la actividad en blanco y un layout con 6 cartas en la mano (maximo de cartas que puede tener el jugador)

---
+++ app/src/main/java/manyosoft/guinyote/ui/JuegoActivity.java
@@ -0,0 +1,16 @@
+package manyosoft.guinyote.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import manyosoft.guinyote.R;
+
+public class JuegoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego);
+    }
+}
